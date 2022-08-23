b4e2bcf474d7341e1b193656c643b01a430de1c5
uyriq <105006434+uyriq@users.noreply.github.com>
2022-08-23 19:48:49 +0300

    upd readme - задание

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-export default class App extends React.Component {
-  render() {
-    return <div className="App"></div>;
-  }
-}
--- src/ui/promo-button/promo-button.js
+++ src/ui/promo-button/promo-button.js
@@ -1,23 +1,23 @@
-import React, { useContext } from 'react';
-import styles from './promo-button.module.css';
-import closeIcon from '../../images/close.svg';
+import React, { useContext } from 'react'
+import styles from './promo-button.module.css'
+import closeIcon from '../../images/close.svg'
 
-import { DiscountContext } from '../../services/appContext';
-import { PromoContext } from '../../services/productsContext';
+import { DiscountContext } from '../../services/appContext'
+import { PromoContext } from '../../services/productsContext'
 
-export const PromoButton = ({ children, extraClass }) => {
-  const { setPromo } = useContext(PromoContext);
-  const { setDiscount } = useContext(DiscountContext);
+export function PromoButton({ children, extraClass }) {
+    const { setPromo } = useContext(PromoContext)
+    const { setDiscount } = useContext(DiscountContext)
 
-  const cancelPromo = () => {
-    setPromo('');
-    setDiscount(0);
-  };
+    const cancelPromo = () => {
+        setPromo('')
+        setDiscount(0)
+    }
 
-  return (
-    <button type="button" className={`${styles.button} ${extraClass}`} onClick={cancelPromo}>
-      {children}
-      <img className={styles.close} src={closeIcon} alt="кнопка закрытия" />
-    </button>
-  );
-};
+    return (
+        <button type="button" className={`${styles.button} ${extraClass}`} onClick={cancelPromo}>
+            {children}
+            <img className={styles.close} src={closeIcon} alt="кнопка закрытия" />
+        </button>
+    )
+}
